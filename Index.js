6aba18245e0a482f91cf5f42faaa94e66b4ef29f
akattamuri <k.aditya.ganesh@gmail.com>
2016-03-09 20:54:45 -0700

    adf

--- Index.js
+++ Index.js
@@ -1,26 +1,5 @@
 //AIzaSyCiU8vZ_QQQXb660jtFlfIxmnA5D87YySg
 
-'use strict'
-const vision = require('node-cloud-vision-api')
- 
-// init with auth
-vision.init({auth: 'AIzaSyCiU8vZ_QQQXb660jtFlfIxmnA5D87YySg'})
- 
-// construct parameters
-const req = new vision.Request({
-  image: new vision.Image({
-      url: 'https://storage.googleapis.com/ocr_storage/Bright_tire_image.JPG'
-  }),
-  features: [
-    new vision.Feature('FACE_DETECTION', 4),
-    new vision.Feature('LABEL_DETECTION', 10),
-  ]
-})
- 
-// send single request
-vision.annotate(req).then((res) => {
-  // handling response
-  console.log(JSON.stringify(res.responses))
-}, (e) => {
-  console.log('Error: ', e)
-})
+var single = require('./singlereq');
+
+single();
